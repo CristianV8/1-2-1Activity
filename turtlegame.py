88a4f772d50cd781a116742aa4626b5892db97c6
Cristian Vintimilla <cristian.vintimilla@vtsd.com>
2021-10-27 12:29:40 -0400

    Updating it

--- turtlegame.py
+++ turtlegame.py
@@ -8,6 +8,9 @@ The timer updating
 #-----import statements-----
 import turtle as trtl
 import random as rand
+import leaderboard as lb
+input("What is your name?")
+
 
 #-----game configuration----
 xMin = 0
@@ -24,6 +27,10 @@ shape = "circle"
 size = 2
 
 fontSetup = ("Arial", 20, "normal")
+leaderboard_file_name = "leaderboard.txt"
+leader_names_list = []
+leader_scores_list = []
+player_name = input("What is your name?")
 
 #-----initialize turtle-----
 t = trtl.Turtle()
@@ -74,10 +81,31 @@ def countdown():
     if timer <= 0:
         counter.write("Time is Up", font=fontSetup)
         timerUp = True
+        manage_leaderboard()
     else:
         counter.write("Time: " + str(timer), font=fontSetup)
         timer -= 1
         counter.getscreen().ontimer(countdown, counterInterval)
+
+
+# manages the leaderboard for top 5 scorers
+def manage_leaderboard():
+    global leader_scores_list
+    global leader_names_list
+    global score
+    global spot
+
+    # load all the leaderboard records into the lists
+    lb.load_leaderboard(leaderboard_file_name, leader_names_list, leader_scores_list)
+
+    # TODO
+    if (len(leader_scores_list) < 5 or score > leader_scores_list[4]):
+        lb.update_leaderboard(leaderboard_file_name, leader_names_list, leader_scores_list, player_name, score)
+        lb.draw_leaderboard(leader_names_list, leader_scores_list, True, spot, score)
+
+    else:
+        lb.draw_leaderboard(leader_names_list, leader_scores_list, False, spot, score)
+
 
 #-----events----------------
 t.onclick(spot_clicked)
