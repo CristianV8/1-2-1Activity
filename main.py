508bffd5fb94cf56203380ebdba7f357246d34b9
CristianV8 <91274235+CristianV8@users.noreply.github.com>
2021-10-30 02:21:15 +0000

    Finished Start Game

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 '''
-Random appearance of the shape on different parts of the screen
-The event of a shape being clicked
-The score updating
-The timer updating
+1. Random appearance of the shape on different parts of the screen
+2. The event of a shape being clicked
+3. The score updating
+4. The timer updating
 '''
 # a121_catch_a_turtle.py
 #-----import statements-----
 import turtle as trtl
 import random as rand
-input("What is your name?")
-
 
 #-----game configuration----
 xMin = 0
@@ -17,71 +15,77 @@ xMax = 112
 yMin = 0
 yMax = 324
 score = 0
-
 timer = 30
-timerUp = False
-counterInterval = 1000
-color = "red"
+timerup = False
+counterinterval = 1000
+fontsetup = ("Arial", 20, "normal")
+color = "blue"
 shape = "circle"
-size = 2
 
 #-----initialize turtle-----
 t = trtl.Turtle()
 t.shape(shape)
-t.turtlesize(size)
 t.fillcolor(color)
 t.penup()
+t.hideturtle()
 
-scoreWriter = trtl.Turtle()
-scoreWriter.penup()
-scoreWriter.goto(300,-300)
+gamestart = trtl.Turtle()
+gamestart.penup()
+gamestart.goto(50, 0)
+gamestart.write("Press to begin!", font=fontsetup)
+
+scorewriter = trtl.Turtle()
+scorewriter.penup()
+scorewriter.goto(300, -300)
+scorewriter.hideturtle()
 
 counter = trtl.Turtle()
 counter.penup()
-counter.goto(-300,-300)
+counter.goto(-300, -300)
+counter.hideturtle()
 
 #-----game functions--------
 def spot_clicked(x, y):
-    t.goto(rand.randint(xMin, xMax), rand.randint(yMin, yMax))
-    scorechange()
-    addcolor()
-    size()
-
+  t.goto(rand.randint(xMin, xMax), rand.randint(yMin, yMax))
+  scorechange()
+  addcolor()
+  size()
 
 def addcolor():
-    colors = ["Red", "Yellow", "Blue", "Black", "Pink", "Purple", "Orange"]
-    t.fillcolor(rand.choice(colors))
-    t.stamp()
-    t.fillcolor(color)
-
+  colorlist = ["Red", "Yellow", "Purple", "Black", "Pink", "Blue", "Orange"]
+  t.fillcolor(rand.choice(colorlist))
+  t.stamp()
+  t.fillcolor(color)
 
 def size():
-    size = [5, 4.5, 4, 3.5, 3, 2.5, 2, 1.5, 1, .5]
-    t.turtlesize(rand.choice(size))
-
+  sizechange = [5, 4.5, 4, 3.5, 3, 2.5, 2, 1.5, 1, .5]
+  t.turtlesize(rand.choice(sizechange))
 
 def scorechange():
-   global score
-   score += 1
-   #print(score)
-   scoreWriter.clear()
-   scoreWriter.write(score,font=fontSetup)
+  global score
+  score += 1
+  scorewriter.clear()
+  scorewriter.write(score, font=fontsetup)
 
+def start_game(x, y):
+  t.showturtle()
+  countdown()
+  gamestart.clear()
 
 def countdown():
-    global timer, timerUp
-    counter.clear()
-    if timer <= 0:
-        counter.write("Time is Up", font=fontSetup)
-        timerUp = True
-    else:
-        counter.write("Time: " + str(timer), font=fontSetup)
-        timer -= 1
-        counter.getscreen().ontimer(countdown, counterInterval)
-
+  global timer, timerUp
+  counter.clear()
+  if timer <= 0:
+      timer -= 1
+      counter.write("Time Is Up!", font=fontsetup)
+      timerUp = True
+  else:
+      counter.write("timer: " + str(timer), font=fontsetup)
+      timer -= 1
+      counter.getscreen().ontimer(countdown, counterinterval)
 #-----events----------------
+gamestart.onclick(start_game)
 t.onclick(spot_clicked)
 wn = trtl.Screen()
-wn.ontimer(countdown, counterInterval)
 wn.bgcolor("green")
 wn.mainloop()
